bffa28a37a130fb3f888f45900b3bd8f2eef0ba9
Eric Sodomka <eric.sodomka@gmail.com>
2012-12-11 13:15:49 -0500

    Used minmax for computing eqa for competitive game

--- src/normalformsolver/BimatrixCocoSolver.java
+++ src/normalformsolver/BimatrixCocoSolver.java
@@ -1,5 +1,6 @@
 package normalformsolver;
 
+import java.util.Arrays;
 import java.util.List;
 
 import props.DiscreteDistribution;
@@ -43,6 +44,14 @@ public class BimatrixCocoSolver<A extends AbstractAction> implements NormalFormS
 			}
 		}
 		
+		System.out.println("player1Payoffs:\n" + print2dArr(player1Payoffs));
+		System.out.println("player2Payoffs:\n" + print2dArr(player2Payoffs));
+		System.out.println("player1CoopPayoffs:\n" + print2dArr(player1CooperativePayoffs));
+		System.out.println("player2CoopPayoffs:\n" + print2dArr(player2CooperativePayoffs));
+		System.out.println("player1CompPayoffs:\n" + print2dArr(player1CompetitivePayoffs));
+		System.out.println("player2CompPayoffs:\n" + print2dArr(player2CompetitivePayoffs));
+
+		
 		// Solve the cooperative game
 		Joint<double[]> cooperativeMixedStrategyPerPlayer = BimatrixHuSolver.solveForMixedStrategies(player1CooperativePayoffs, player2CooperativePayoffs);
 		double[] player1CooperativeStrategy = cooperativeMixedStrategyPerPlayer.getForPlayer(player1Idx);
@@ -52,14 +61,82 @@ public class BimatrixCocoSolver<A extends AbstractAction> implements NormalFormS
 		double player1CooperativeExpectedPayoff = BimatrixHuSolver.getExpectedPayoffsForPlayer(player1CooperativePayoffs, jointCooperativeStrategy);
 		double player2CooperativeExpectedPayoff = BimatrixHuSolver.getExpectedPayoffsForPlayer(player2CooperativePayoffs, jointCooperativeStrategy);
 		
+		System.out.println("jointCooperativeStrategy:\n" + print2dArr(jointCooperativeStrategy));
+
 		// Solve the competitive game.
 		Joint<double[]> competitiveMixedStrategyPerPlayer = BimatrixHuSolver.solveForMixedStrategies(player1CompetitivePayoffs, player2CompetitivePayoffs);
-		double[] player1CompetitiveStrategy = competitiveMixedStrategyPerPlayer.getForPlayer(player1Idx);
-		double[] player2CompetitiveStrategy = competitiveMixedStrategyPerPlayer.getForPlayer(player2Idx);
+		
+		// Buggy. Use minmax / maxmin
+		boolean minmaxHack = true;
+		double[] player1CompetitiveStrategy;
+		double[] player2CompetitiveStrategy;
+		if (!minmaxHack) { 
+			player1CompetitiveStrategy = competitiveMixedStrategyPerPlayer.getForPlayer(player1Idx);
+			player2CompetitiveStrategy = competitiveMixedStrategyPerPlayer.getForPlayer(player2Idx);
+		}
+		
+		////////////// START HACK
+		else {
+		// minmax
+		int player1BestAction = -1;
+		double player1BestVal = Double.NEGATIVE_INFINITY;
+		for (int a1=0; a1<numPlayer1Actions; a1++) {
+			// Get best a2 value for this action
+			int bestA2 = -1;
+			double bestA2Val = Double.NEGATIVE_INFINITY;			
+			for (int a2=0; a2<numPlayer2Actions; a2++) {
+				if (player2CompetitivePayoffs[a1][a2] > bestA2Val) {
+					bestA2Val = player2CompetitivePayoffs[a1][a2];
+					bestA2 = a2;
+				}
+			}
+			// See if a1 is still better than any other action if a2 minimizes a1.
+			if (player1CompetitivePayoffs[a1][bestA2] > player1BestVal) {
+				player1BestVal = player1CompetitivePayoffs[a1][bestA2];
+				player1BestAction = a1;
+			}
+		}
+		player1CompetitiveStrategy = new double[numPlayer1Actions];
+		player1CompetitiveStrategy[player1BestAction] = 1.0;
+		
+		// maxmin
+		int player2BestAction = -1;
+		double player2BestVal = Double.NEGATIVE_INFINITY;
+		for (int a2=0; a2<numPlayer2Actions; a2++) {
+			// Get best a1 value for this action
+			int bestA1 = -1;
+			double bestA1Val = Double.NEGATIVE_INFINITY;			
+			for (int a1=0; a1<numPlayer1Actions; a1++) {
+				if (player1CompetitivePayoffs[a1][a2] > bestA1Val) {
+					bestA1Val = player1CompetitivePayoffs[a1][a2];
+					bestA1 = a1;
+				}
+			}
+			// See if a2 is still better than any other action if a1 minimizes a2.
+			if (player2CompetitivePayoffs[bestA1][a2] > player2BestVal) {
+				player2BestVal = player2CompetitivePayoffs[bestA1][a2];
+				player2BestAction = a2;
+			}
+		}
+		player2CompetitiveStrategy = new double[numPlayer2Actions];
+		player2CompetitiveStrategy[player2BestAction] = 1.0;
+		}
+		//////////////////////// END HACK
+				
 		double[][] jointCompetitiveStrategy = BimatrixHuSolver.getDistributionOverJointActions(player1CompetitiveStrategy, player2CompetitiveStrategy);
 		// Get expected payoffs if agents play these strategies. 
+		
+		
+		
+		
 		double player1CompetitiveExpectedPayoff = BimatrixHuSolver.getExpectedPayoffsForPlayer(player1CompetitivePayoffs, jointCompetitiveStrategy);
 		double player2CompetitiveExpectedPayoff = BimatrixHuSolver.getExpectedPayoffsForPlayer(player2CompetitivePayoffs, jointCompetitiveStrategy);
+
+		
+		
+		System.out.println("jointCompetitiveStrategy:\n" + print2dArr(jointCompetitiveStrategy));
+
+		
 		
 		// Compute coco values
 		double player1CoCoVal = player1CooperativeExpectedPayoff + player1CompetitiveExpectedPayoff;
@@ -78,12 +155,25 @@ public class BimatrixCocoSolver<A extends AbstractAction> implements NormalFormS
 		Joint<Double> transferPayments = new Joint<Double>();
 		transferPayments.add(player1TransferPayments);
 		transferPayments.add(player2TransferPayments);
+
+		System.out.println("p1Coco=" + player1CoCoVal + ", p2Coco=" + player2CoCoVal);
+		System.out.println("p1ActualPayoff=" + player1ActualExpectedPayoff + ", p2ActualPayoff=" + player2ActualExpectedPayoff);
+		System.out.println("p1Transfer=" + player1TransferPayments + ", p2Transfer=" + player2TransferPayments);
 		
 		// Solution is to follow the cooperative strategy.
 		DiscreteDistribution<Joint<A>> jointStrategy = BimatrixHuSolver.getJointStrategyFromIndependentStrategies(normalFormGame, player1CooperativeStrategy, player2CooperativeStrategy);
 		GameSolution<A> solution = new UncorrelatedGameSolution<A>(normalFormGame, jointStrategy, expectedPayoffs, transferPayments);
 
 		return solution;
+	}
+	
+	
+	public static String print2dArr(double[][] arr) {
+		StringBuffer sb = new StringBuffer();
+		for (int i=0; i<arr.length; i++) {
+			sb.append(Arrays.toString(arr[i])+"\n");
+		}
+		return sb.toString();
 	}
 
 }
